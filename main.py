ab96c03b6509c5030b79619d7ec0c5476aad7f3c
M-Enzinger <104521938+M-Enzinger@users.noreply.github.com>
2023-04-23 17:48:21 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,22 @@
 import streamlit as st
+from ultralytics import YOLO
 
-st.markdown('Hello')
+# Load a model
+model = YOLO('yolov8n.pt')  # load an official detection model
+model = YOLO('yolov8n-seg.pt')  # load an official segmentation model
+model = YOLO('path/to/best.pt')  # load a custom model
+
+# Track with the model
+sfa = st.file_uploader("", type=['mp4', 'avi', 'mov'])
+col1, col2, col3 = st.columns(3)
+with col2:
+    button_sfa = st.button('Analyse')
+if button_sfa:
+  with tempfile.NamedTemporaryFile(delete=False) as tmp_file:
+              fp = Path(tmp_file.name)
+              fp.write_bytes(sfa.getvalue())
+  results = model.track(source=tmp_file.name, show=True) 
+  video_file = open('myvideo.mp4', 'rb')
+  video_bytes = results.read()
+
+  st.video(video_bytes)
